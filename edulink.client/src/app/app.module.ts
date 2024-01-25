94a4bbad740cfa31fa79aa3a2e0d7ece93cbd571
Geoffrey Muzard <muzardgeoffrey@gmail.com>
2024-01-25 12:32:54 +0100

    correction url identification

--- edulink.client/src/app/app.module.ts
+++ edulink.client/src/app/app.module.ts
@@ -2,9 +2,9 @@ import { Routes } from '@angular/router';
 import { AuthGuard } from './auth.guard';
 
 export const routes: Routes = [
+  { path: '', loadChildren: () => import('./identification/identification.route') },
   { path: 'home', loadComponent: () => import('./home/home.component').then(module => module.HomeComponent), title: "home" },
-  { path: 'profile', loadComponent: () => import('./profile/profile.component').then(module => module.ProfileComponent), title: "profile",canActivate: [AuthGuard] },
-  { path: 'identification', loadChildren: () => import('./identification/identification.route') },
+  { path: 'profile', loadComponent: () => import('./profile/profile.component').then(module => module.ProfileComponent), title: "profile", canActivate: [AuthGuard] },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
 
 ];
