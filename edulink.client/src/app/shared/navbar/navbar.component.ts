2fe5694de3b0543b07cd144e667cbe65ed087d46
Geoffrey Muzard <muzardgeoffrey@gmail.com>
2024-02-27 11:41:55 +0100

    Creation de la navbar

--- edulink.client/src/app/shared/navbar/navbar.component.ts
+++ edulink.client/src/app/shared/navbar/navbar.component.ts
@@ -1,45 +1,29 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet, Router, NavigationEnd, NavigationStart } from '@angular/router';
-import { Location, PopStateEvent } from '@angular/common';
+import { Component, ViewChild } from '@angular/core';
+import { RouterLink, RouterOutlet, Router } from '@angular/router';
+import { MatMenuModule, MatMenuTrigger } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css'],
   standalone: true,
-  imports: [RouterOutlet, RouterLink]
+  imports: [RouterOutlet, RouterLink, MatMenuModule, MatIconModule]
 })
 export class NavbarComponent {
+  @ViewChild(MatMenuTrigger)
+  trigger!: MatMenuTrigger;
   public isCollapsed = true;
   private lastPoppedUrl: string | undefined;
   private yScrollStack: number[] = [];
 
-  constructor(public location: Location, private router: Router) {
+  constructor(private router: Router) {
   }
 
   ngOnInit() {
-    this.router.events.subscribe((event) => {
-      this.isCollapsed = true;
-      if (event instanceof NavigationStart) {
-        if (event.url != this.lastPoppedUrl)
-          this.yScrollStack.push(window.scrollY);
-      } else if (event instanceof NavigationEnd) {
-        if (event.url == this.lastPoppedUrl) {
-          this.lastPoppedUrl = undefined;
-          let last = this.yScrollStack.pop();
-          if (last != undefined) {
-            window.scrollTo(0, last);
-          }
-        } else
-          window.scrollTo(0, 0);
-      }
-    });
-    this.location.subscribe((ev: PopStateEvent) => {
-      this.lastPoppedUrl = ev.url;
-    });
   }
 
-  ChangeCollapse() {
-    this.isCollapsed = !this.isCollapsed;
+  someMethod() {
+    this.trigger.openMenu();
   }
 }
--- edulink.client/src/main.ts
+++ edulink.client/src/main.ts
@@ -8,13 +8,16 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { withInterceptorsFromDi, provideHttpClient } from '@angular/common/http';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 import { FooterComponent } from './app/shared/footer/footer.component';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { MatMenuTrigger } from '@angular/material/menu';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    importProvidersFrom(BrowserModule, FormsModule, ReactiveFormsModule),
+    importProvidersFrom(BrowserModule, FormsModule, ReactiveFormsModule, MatMenuTrigger),
     provideHttpClient(withInterceptorsFromDi()),
     provideRouter(routes),
-    FooterComponent
+    FooterComponent,
+    provideAnimations()
   ]
 })
   .catch(err => console.error(err));
